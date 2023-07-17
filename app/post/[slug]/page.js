7e7ab523705dcb8b76819df89da6f434f773cb3d
Nouman Qamar <84765475+noumanqamar450@users.noreply.github.com>
2023-07-17 22:26:06 +0500

    13th update

--- app/post/[slug]/page.js
+++ app/post/[slug]/page.js
@@ -1,9 +1,14 @@
-'use client'
 import CommentCard from "@/app/components/CommentCard";
 import CommentForm from "@/app/components/CommentForm";
 import Image from "next/image";
 import { Suspense } from "react";
 import Loading from "./loading";
+
+async function getPostAll() {
+    const res = await fetch(`http://localhost:3000/api/post`);
+    const post = await res.json();
+    return post.data;
+}
 
 async function getPost(slug) {
     const res = await fetch(`http://localhost:3000/api/post?slug=${slug}`);
@@ -98,4 +103,12 @@ export default async function Home({ params }) {
             </div>
         </Suspense>
     )
+}
+
+export async function generateStaticParams() {
+    const posts = await getPostAll();
+
+    return posts.map((post) => ({
+        slug: post.slug,
+    }))
 }
